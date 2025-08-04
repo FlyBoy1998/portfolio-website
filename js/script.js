096ba4d7c06468c57ab5eec135d4fd0a08c16d67
Florin Miron <mironflorin123@yahoo.com>
2025-08-04 11:18:07 +0100

    Implement the animation functionality on each off-screen navigation links once the user clicks the hamburger button

--- js/script.js
+++ js/script.js
@@ -3,15 +3,34 @@ const offScreenNav = document.getElementById("off-screen-nav");
 const offScreenNavCloseBtn = document.querySelector(
   ".off-screen-nav__close-btn"
 );
+const offScreenNavLinks = document.querySelectorAll(
+  ".off-screen-nav__links--link"
+);
+
+function addFadeLeftAnimation() {
+  let animationDelay = 0.2;
+
+  offScreenNavLinks.forEach((link) => {
+    link.classList.add("fade-left");
+    link.style.animation = `fadeLeft 0.6s ease-out ${animationDelay}s 1 normal forwards running`;
+    animationDelay += 0.2;
+  });
+}
+
+function removeFadeLeftAnimation() {
+  offScreenNavLinks.forEach((link) => (link.style.animation = ""));
+}
 
 function displayOffScreenNav() {
   hamburgerBtn.setAttribute("aria-expanded", true);
   offScreenNav.classList.add("active");
+  addFadeLeftAnimation();
 }
 
 function hideOffScreenNav() {
   hamburgerBtn.setAttribute("aria-expanded", false);
   offScreenNav.classList.remove("active");
+  removeFadeLeftAnimation();
 }
 
 function setTippyProps(element, theme, content) {

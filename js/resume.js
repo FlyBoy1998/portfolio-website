32d7d320b40a4006b22258796a5a951377b2aff7
Florin Miron <mironflorin123@yahoo.com>
2025-08-08 21:19:08 +0100

    Implemnent the functionality of displaying tab content based on the button the user clicks

--- js/resume.js
+++ js/resume.js
@@ -2,6 +2,8 @@ const resumeHeading = document.querySelector(".resume-heading");
 const resumeSubtextHighlight = document.querySelector(
   ".resume-subtext__highlight"
 );
+const tabButtons = document.querySelectorAll(".tab-buttons__button");
+const tabContent = document.querySelectorAll(".tab-content__tab");
 
 const typed = new Typed(resumeSubtextHighlight, {
   strings: [
@@ -18,3 +20,23 @@ const typed = new Typed(resumeSubtextHighlight, {
   loop: true,
   loopCount: Infinity,
 });
+
+function selectResumeTab() {
+  tabButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const buttonTab = button.dataset.buttonTab;
+
+      tabButtons.forEach((tabBtn) => {
+        tabBtn.classList.remove("active");
+      });
+      button.classList.add("active");
+
+      tabContent.forEach((tab) => {
+        tab.classList.remove("active");
+        document.getElementById(buttonTab).classList.add("active");
+      });
+    });
+  });
+}
+
+selectResumeTab();

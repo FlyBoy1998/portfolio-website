f1e6a03b6ac394deb9afd807aa35a4f2b5da9ace
Florin Miron <mironflorin123@yahoo.com>
2025-08-11 13:41:35 +0100

    Create technologies js file and implement and array of objects containing each technology

---
+++ js/technologies.js
@@ -0,0 +1,68 @@
+const technologies = [
+  {
+    id: 1,
+    technologyName: "HTML5",
+    technologyIcon: "html5-icon.png",
+    altText: "HTML5 Icon",
+  },
+  {
+    id: 2,
+    technologyName: "CSS3",
+    technologyIcon: "css3-icon.png",
+    altText: "CSS3 Icon",
+  },
+  {
+    id: 3,
+    technologyName: "JavaScript",
+    technologyIcon: "javascript-icon.png",
+    altText: "JavaScript Icon",
+  },
+  {
+    id: 4,
+    technologyName: "Python",
+    technologyIcon: "python-icon.png",
+    altText: "Python Icon",
+  },
+  {
+    id: 5,
+    technologyName: "Bootstrap5",
+    technologyIcon: "bootstrap5-icon.png",
+    altText: "Boostrap5 Icon",
+  },
+  {
+    id: 6,
+    technologyName: "Tailwind CSS",
+    technologyIcon: "tailwind-css-icon.png",
+    altText: "Tailwind CSS Icon",
+  },
+  {
+    id: 7,
+    technologyName: "SASS",
+    technologyIcon: "sass-icon.png",
+    altText: "SASS Icon",
+  },
+  {
+    id: 8,
+    technologyName: "Flask",
+    technologyIcon: "flask-icon.png",
+    altText: "Flask Icon",
+  },
+  {
+    id: 9,
+    technologyName: "Django",
+    technologyIcon: "django-icon.png",
+    altText: "Django Icon",
+  },
+  {
+    id: 10,
+    technologyName: "Git",
+    technologyIcon: "git-icon.png",
+    altText: "Git Icon",
+  },
+  {
+    id: 11,
+    technologyName: "GitHub",
+    technologyIcon: "github-icon.png",
+    altText: "GitHub Icon",
+  },
+];

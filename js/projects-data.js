018f4f32b58647f6d778c66ef45e08cdab8490f7
Florin Miron <mironflorin123@yahoo.com>
2025-09-08 10:39:04 +0100

    Add the projects data js file

---
+++ js/projects-data.js
@@ -0,0 +1,150 @@
+const projectsData = [
+  {
+    id: 1,
+    title: "Urban Steakhouse",
+    excerpt:
+      "This steakhouse website caters to food enthusiasts and tourists seeking top dining experiences.",
+    description:
+      "This is a steakhouse restaurant's website created for food enthusiasts looking for amazing dining experiences, tourists looking for the best local restaurants, but it can also be an inspiration for developers as it is built with a modern approach in terms of web design using the latest features of front end development.",
+    technologies: [
+      {
+        technologyName: "HTML5",
+        technologyIcon: "/html5-icon.png",
+      },
+      {
+        technologyName: "CSS3",
+        technologyIcon: "/css3-icon.png",
+      },
+      {
+        technologyName: "Bootstrap5",
+        technologyIcon: "/bootstrap5-icon.png",
+      },
+    ],
+    lightHouseScores: [99, 100, 100, 100],
+    links: {
+      liveVersion:
+        "https://florinmiron98.github.io/urban-steakhouse-restaurant/",
+      sourceCode:
+        "https://github.com/FlorinMiron98/urban-steakhouse-restaurant",
+    },
+    mockups: "urban-steakhouse-mockup.png",
+    image: "urban-steakhouse.jpg",
+  },
+  {
+    id: 2,
+    title: "Jet-Set-Go",
+    excerpt:
+      "This is a flights tickets booking website designed to facilitate seamless flight search and booking.",
+    description:
+      "This is a flights tickets booking website designed to facilitate seamless flight search and booking. The website uses the booking API to provide real-time flight data. Users can search for flights based on their destination, dates, and preferences, and complete bookings through an easy-to-use submission form.",
+    technologies: [
+      {
+        technologyName: "HTML5",
+        technologyIcon: "html5-icon.png",
+      },
+      {
+        technologyName: "CSS3",
+        technologyIcon: "css3-icon.png",
+      },
+      {
+        technologyName: "Bootstrap5",
+        technologyIcon: "bootstrap5-icon.png",
+      },
+      {
+        technologyName: "JavaScript",
+        technologyIcon: "javascript-icon.png",
+      },
+    ],
+    lightHouseScores: [92, 95, 96, 91],
+    links: {
+      liveVersion: "https://jet-set-go-florin-miron.netlify.app/",
+      sourceCode: "https://github.com/FlorinMiron98/Jet-Set-Go",
+    },
+    mockups: "jet-set-go-mockup.png",
+    image: "jet-set-go.jpg",
+  },
+  {
+    id: 3,
+    title: "CineData",
+    excerpt:
+      "An IMDb-inspired app to browse, search, and view movie and TV show details with a responsive design.",
+    description:
+      "This is a web application inspired by IMDb, built to allow users to browse, search, and view detailed information about movies and TV shows. It features a dynamic user interface, a searchable movie database, and responsive design for an optimal experience across devices.",
+    technologies: [
+      {
+        technologyName: "HTML5",
+        technologyIcon: "html5-icon.png",
+      },
+      {
+        technologyName: "CSS3",
+        technologyIcon: "css3-icon.png",
+      },
+      {
+        technologyName: "Bootstrap5",
+        technologyIcon: "bootstrap5-icon.png",
+      },
+      {
+        technologyName: "JavaScript",
+        technologyIcon: "javascript-icon.png",
+      },
+      {
+        technologyName: "Python",
+        technologyIcon: "python-icon.png",
+      },
+      {
+        technologyName: "Flask",
+        technologyIcon: "flask-icon.png",
+      },
+    ],
+    lightHouseScores: [100, 99, 100, 100],
+    links: {
+      liveVersion: "https://cinedata-app-5c6257798b71.herokuapp.com/",
+      sourceCode: "https://github.com/FlorinMiron98/CineData",
+    },
+    mockups: "cinedata-mockup.png",
+    image: "cinedata.jpg",
+  },
+  {
+    id: 4,
+    title: "ReLeaf",
+    excerpt:
+      "A platform for donations and community engagement to support global reforestation - heal the earth",
+    description:
+      "This is a web application built to support environmental action through community-driven reforestation efforts. The platform features a clean, responsive design that makes it easy to explore updates and contribute toward restoring forests and fighting climate change.",
+    technologies: [
+      {
+        technologyName: "HTML5",
+        technologyIcon: "html5-icon.png",
+      },
+      {
+        technologyName: "CSS3",
+        technologyIcon: "css3-icon.png",
+      },
+      {
+        technologyName: "Bootstrap5",
+        technologyIcon: "bootstrap5-icon.png",
+      },
+      {
+        technologyName: "JavaScript",
+        technologyIcon: "javascript-icon.png",
+      },
+      {
+        technologyName: "Python",
+        technologyIcon: "python-icon.png",
+      },
+      {
+        technologyName: "Django",
+        technologyIcon: "django-icon.png",
+      },
+    ],
+    lightHouseScores: [99, 95, 100, 100],
+    links: {
+      liveVersion: "https://re-leaf-a6d5eec7dd3a.herokuapp.com/",
+      sourceCode: "https://github.com/FlorinMiron98/ReLeaf",
+    },
+    mockups: "releaf-mockup.png",
+    image: "releaf.jpg",
+  },
+];
+
+export default projectsData;
